63a06e242d9e03acc8750cd8ef61d4569311ca36
František Hába <hello@frantisekhaba.com>
2011-07-24 22:51:17 +0100

    Updated test ‘isIpv6’

--- tests/isIpv6.js
+++ tests/isIpv6.js
@@ -8,6 +8,9 @@ var Robb = require('../src/robb.js');
  */
 exports.isIpv6 = function(test) {
 
+  test.strictEqual( Robb.isIp('2001:0db8:85a3:0000:0000:8a2e:0370:7334'), true );
+  test.strictEqual( Robb.isIpv4('2001:0db8:85a3:0000:0000:8a2e:0370:7334'), false );
+  test.strictEqual( Robb.isIpv6('2001:0db8:85a3:0000:0000:8a2e:0370:7334'), true );
   test.done();
 
 };
